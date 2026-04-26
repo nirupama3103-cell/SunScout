1e084e0a611606e9583a56973701d8b4a00fad5a
nirupama3103-cell <nirupama3103@gmail.com>
2026-04-26 01:09:25 -0700

    Update useActiveColor.js

--- useActiveColor.js
+++ useActiveColor.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { TAB_COLORS } from '../lib/constants.js'
+import { TAB_COLORS } from './constants.js'
 
 /**
  * Syncs the CSS custom properties --active-color, --active-r/g/b
@@ -7,11 +7,15 @@ import { TAB_COLORS } from '../lib/constants.js'
  */
 export function useActiveColor(day) {
   useEffect(() => {
-    const c = TAB_COLORS[day]
-    const root = document.documentElement
-    root.style.setProperty('--active-color', c.hex)
-    root.style.setProperty('--active-r', c.r)
-    root.style.setProperty('--active-g', c.g)
-    root.style.setProperty('--active-b', c.b)
+    // If the day doesn't exist in our colors, default to 'today'
+    const c = TAB_COLORS[day] || TAB_COLORS['today']
+    
+    if (c) {
+      const root = document.documentElement
+      root.style.setProperty('--active-color', c.hex)
+      root.style.setProperty('--active-r', c.r)
+      root.style.setProperty('--active-g', c.g)
+      root.style.setProperty('--active-b', c.b)
+    }
   }, [day])
 }
