b3ab90891e5a05ebc218403fcf05056478e4e852
nirupama3103-cell <nirupama3103@gmail.com>
2026-04-27 12:14:14 -0700

    Update places.js

--- api/places.js
+++ api/places.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
         locationRestriction: {
           circle: { 
             center: { latitude: lat, longitude: lon }, 
-            radius: 80467 // This is 50 miles in meters
+            radius: 80467 // 50 miles in meters
           } 
         }
       })
