7622a0654105f6b6c047e1fba43da3184fce22ae
nirupama3103-cell <nirupama3103@gmail.com>
2026-04-27 16:34:44 -0700

    Create places.js

---
+++ places.js
@@ -0,0 +1,9 @@
+export async function fetchNearbyPlaces(lat, lon, type = 'park', radius = 8000) {
+  const response = await fetch('/api/places', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ lat, lon, type, radius }),
+  });
+  const data = await response.json();
+  return data.places || [];
+}
