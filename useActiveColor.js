0cd3db1bfc4b709abd7892ae460bc20c978397ea
nirupama3103-cell <nirupama3103@gmail.com>
2026-04-27 16:32:50 -0700

    Update useActiveColor.js

--- useActiveColor.js
+++ useActiveColor.js
@@ -1,15 +1,9 @@
 import { useEffect } from 'react'
 import { TAB_COLORS } from './constants.js'
 
-/**
- * Syncs the CSS custom properties --active-color, --active-r/g/b
- * on :root whenever the active day tab changes.
- */
 export function useActiveColor(day) {
   useEffect(() => {
-    // If the day doesn't exist in our colors, default to 'today'
     const c = TAB_COLORS[day] || TAB_COLORS['today']
-    
     if (c) {
       const root = document.documentElement
       root.style.setProperty('--active-color', c.hex)
