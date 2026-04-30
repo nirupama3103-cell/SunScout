16bdad8af88a7cb58708f688e0b5c9cd046a9cad
nirupama3103-cell <nirupama3103@gmail.com>
2026-04-30 18:01:25 +0000

    Update: Added San Joaquin and new Row 3 tab names

--- constants.js
+++ constants.js
@@ -2,19 +2,20 @@ export const REGIONS = {
   SC: { name: 'Santa Clara', cities: ['Sunnyvale', 'San Jose', 'Cupertino', 'Saratoga', 'Mountain View', 'Palo Alto'] },
   SM: { name: 'San Mateo', cities: ['San Mateo', 'Redwood City', 'Burlingame', 'Menlo Park', 'Foster City'] },
   ALAMEDA: { name: 'Alameda', cities: ['Oakland', 'Fremont', 'Berkeley', 'Pleasanton', 'Dublin'] },
-  SF: { name: 'San Francisco', cities: ['Downtown', 'Sunset', 'Richmond', 'Mission', 'Marina'] }
+  SF: { name: 'San Francisco', cities: ['Downtown', 'Sunset', 'Richmond', 'Mission', 'Marina'] },
+  SJ: { name: 'San Joaquin', cities: ['Stockton', 'Lodi', 'Tracy', 'Manteca', 'Lathrop'] }
 };
 
 export const WALLETS = {
-  ALL: 'All Fun',
-  FREE: 'Free Fun',
-  SUMMER: 'Summer',
-  PAID: 'Paid Camps'
+  ALL: 'Indoor Activities',
+  FREE: 'Weekend Activities',
+  SUMMER: 'Summer Free Fun',
+  PAID: 'Paid Activities'
 };
 
 export const MOODS = {
-  RUN: 'Run Wild 🏃‍♂️',
+  RUN: 'Run Wild 🏃‍♀️',
   COOL: 'Cool Down 💦',
   SMART: 'Smarty Pants 🧠',
-  BREAK: 'Off Duty ☕'
+  BREAK: 'Off Duty 😴'
 };
