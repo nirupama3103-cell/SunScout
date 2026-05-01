4924df022e436f4a15c6125c741e73c082fd1c90
nirupama3103-cell <nirupama3103@gmail.com>
2026-05-01 18:07:19 +0000

    build: correct css import to index.css

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { fetchActivitiesForCity, CITIES_BY_COUNTY } from './activities';
-import './App.css';
+import './index.css';
 
 function App() {
   const [activeCounty, setActiveCounty] = useState("Santa Clara");
